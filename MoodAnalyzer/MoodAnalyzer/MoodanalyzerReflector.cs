016b9648cb2c9c6e8697be5f068a14673ace7ec0
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-13 17:15:57 +0530

    Refactor mood analyzer reflector

--- MoodAnalyzer/MoodAnalyzer/MoodanalyzerReflector.cs
+++ MoodAnalyzer/MoodAnalyzer/MoodanalyzerReflector.cs
@@ -8,19 +8,8 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyzer
 {
-    public class MoodAnalyzerFactory
+    public class MoodAnalyzerReflector
     {
-        /// <summary>
-        /// CreateMoodAnalyse method to create object of MoodAnalyse class.
-        /// </summary>
-        /// <param name="className">Name of the class.</param>
-        /// <param name="constructorName">Name of the constructor.</param>
-        /// <returns></returns>
-        /// <exception cref="MoodAnalyzer.MoodAnalyzerException">
-        /// Class not found
-        /// or
-        /// Constructor not found
-        /// </exception>
         public static object CreateMoodAnalyse(string className, string constructorName)
         {
             // create the pattern and checks whether constructor name and class name are equal
@@ -80,6 +69,62 @@ namespace MoodAnalyzer
             catch (Exception e)
             {
                 return e;
+            }
+        }
+        /// <summary>
+        /// dry principle optional variables
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static object CreateMoodAnalyserOptionalVariable(string className, string constructorName, string message, string msg = "I am optional variable")
+        {
+            Type type = Type.GetType(className);
+            try
+            {
+                if (type.FullName.Equals(className) || type.Name.Equals(className))
+                {
+                    if (type.Name.Equals(constructorName))
+                    {
+                        ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                        object instance = info.Invoke(new object[] { message });
+                        return instance;
+                    }
+                    else
+                    {
+                        throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_CONSTRUCTOR, "Constructor not found");
+                    }
+                }
+                else
+                {
+                    throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                }
+            }
+            catch (Exception e)
+            {
+                return e;
+            }
+        }
+        /// <summary>
+        /// Use Reflection to invoke method
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="methodName"></param>
+        /// <returns></returns>
+        public static string InvokeAnalyseMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("MoodAnalyzer.AnalyzeMood");
+                object moodAnalyseObject = MoodAnalyzerReflector.CreateMoodAnalyserParameterizedConstructor("MoodAnalyzer.AnalyzeMood", "AnalyzeMood", message);
+                MethodInfo methodInfo = type.GetMethod(methodName);
+                object mood = methodInfo.Invoke(moodAnalyseObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_METHOD, "No method found");
             }
         }
     }
